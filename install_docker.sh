dc36df35d009c4329926859834ae3e6f93abefea
markrichmond1994 <60485080+markrichmond1994@users.noreply.github.com>
2020-05-01 13:30:32 -0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -9,6 +9,17 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 sudo apt-get install -y httping
 sudo apt-get install -y jq
 
+sudo apt-get install nfs-kernel-server
+sudo mkdir -p /mnt/sharedfolder
+sudo chown nobody:nogroup /mnt/sharedfolder
+sudo chmod 777 /mnt/sharedfolder
+sudo nano /etc/exports
+/mnt/sharedfolder 192.168.56.1/24(rw,sync,no_subtree_check)
+sudo exportfs -a
+sudo systemctl restart nfs-kernel-server
+sudo ufw allow from 192.168.56.1/24 to any port nfs
+sudo ufw status
+
 # the username needs to be changed
 while IFS= read -r line; do
   sudo usermod -aG docker $line
